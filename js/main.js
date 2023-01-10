17f23ef713cc8ecf0d8f27eef15515132299f0bc
Axel Duran <duranaxel78@gmail.com>
2023-01-09 22:33:36 -0300

    hice cambios en las variables y puse los do while en los ingresos de nombre y notas

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
-//CICLO
+let alumno;
+let nota1;
+let nota2;
+let nota3;
 
-for(let i = 0; i < 3; i++){
-    const nombre = prompt("Por favor, ingrese su nombre")
-    const edad = prompt("Ingrese su edad")
-    const mesNacimiento = prompt("Ingrese su mes de nacimiento")
-    const anioNacimiento = prompt("Ingrese su año de nacimiento")
+do{
+    alumno = prompt("Ingrese nombre")
+}while(!isNaN(alumno))
 
-    console.log("La edad del estudiante " + nombre + " es " + edad + " y nació en " + mesNacimiento + " de " + anioNacimiento)
-}
+do{
+    nota1 = parseInt(prompt("Ingrese la nota 1"))
+}while(isNaN(nota1))
 
-//CONDICIONAL
+do{
+    nota2 = parseInt(prompt("Ingrese la nota 2"))
+}while(isNaN(nota2))
 
-let alumno = prompt("Ingrese su nombre")
-let nota1 = parseInt(prompt("Ingrese nota 1"))
-let nota2 = parseInt(prompt("Ingrese nota 2"))
-let nota3 = parseInt(prompt("Ingrese nota 3"))
+do{
+    nota3 = parseInt(prompt("Ingrese la nota 3"))
+}while(isNaN(nota3))
+
 
 function calcularNota (alumno, nota1, nota2, nota3){
     let sumaNota = nota1 + nota2 + nota3
