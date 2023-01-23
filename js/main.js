babb12f374ecaf46cfc8fea262d1c4e4fb7a59ff
Axel Duran <duranaxel78@gmail.com>
2023-01-23 18:24:53 -0300

    agregué un array de objeto y el método de búsqueda filter

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ let alumno;
 let nota1;
 let nota2;
 let nota3;
+let promedio;
 
 do{
     alumno = prompt("Ingrese nombre")
@@ -9,20 +10,20 @@ do{
 
 do{
     nota1 = parseInt(prompt("Ingrese la nota 1"))
-}while(isNaN(nota1))
+}while(isNaN(nota1)||!(nota1 > 0 && nota1 < 11))
 
 do{
     nota2 = parseInt(prompt("Ingrese la nota 2"))
-}while(isNaN(nota2))
+}while(isNaN(nota2)||!(nota2 > 0 && nota2 < 11))
 
 do{
     nota3 = parseInt(prompt("Ingrese la nota 3"))
-}while(isNaN(nota3))
+}while(isNaN(nota3)||!(nota3 > 0 && nota3 < 11))
 
 
 function calcularNota (alumno, nota1, nota2, nota3){
     let sumaNota = nota1 + nota2 + nota3
-    let promedio = sumaNota/3
+    promedio = sumaNota/3
 
     if (promedio >= 7){
         alert("El estudiante " + alumno + " ha promocionado con " + promedio);
@@ -33,4 +34,19 @@ function calcularNota (alumno, nota1, nota2, nota3){
     }
 }
 
-calcularNota(alumno, nota1, nota2, nota3)
+const estudiante = []
+
+estudiante.push({
+     nombre: alumno,
+     nota1: nota1,
+     nota2: nota2,
+     nota3: nota3,
+     promedio: (nota1 + nota2 + nota3) / 3,
+});
+
+console.log(estudiante);
+
+calcularNota(alumno, nota1, nota2, nota3);
+
+let aprobados = estudiante.filter(alumno => alumno.promedio >= 7);
+console.log(aprobados);
